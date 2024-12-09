6a1fc70bb2ddc036c1cc1447ef0fad21bd009dc1
cezarfbc <cezarfbc@gmail.com>
2024-12-09 11:14:59 -0300

    test: add new end-to-end tests for authentication routes and remove obsolete test file

---
+++ test/auth/refresh-token.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../src/app.module';
+
+describe('AuthController from AppModule (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/auth/refresh-token/ (POST)', () => {
+    return request(app.getHttpServer())
+      .get('/auth/refresh-token/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
