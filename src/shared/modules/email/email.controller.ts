bc527675f465b2800f64c3870d08eede56a0b98f
cezarfbc <cezarfbc@gmail.com>
2025-01-18 12:21:38 -0300

    feat: add email sender endpoint to EmailController with IsPublicRoute decorator

--- src/shared/modules/email/email.controller.ts
+++ src/shared/modules/email/email.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { ApiTags } from '@nestjs/swagger';
+import { IsPublicRoute } from 'src/shared/decorators/is_public_route.decorator';
+import { EmailServiceDto } from './dto/email.service.dto';
 
 @ApiTags('email')
 @Controller('email')
 export class EmailController {
   @Inject()
   private readonly _emailService: EmailService;
+
+  @IsPublicRoute()
+  @Post('email-sender')
+  public async emailSender(@Body() input: EmailServiceDto): Promise<void> {}
 }
