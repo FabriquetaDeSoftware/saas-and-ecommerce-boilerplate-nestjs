1cb3de25e46fcc5cadae147d6a8041acc12ff3d7
cezarfbc <cezarfbc@gmail.com>
2025-01-27 11:27:48 -0300

    refactor: uncomment port mapping for PostgreSQL in Docker Compose and update email example in EmailDto

--- src/modules/auth/application/dto/email.dto.ts
+++ src/modules/auth/application/dto/email.dto.ts
@@ -4,7 +4,7 @@ import { IsEmail, IsNotEmpty } from 'class-validator';
 export class EmailDto {
   @ApiProperty({
     description: 'Email of the user to execute action',
-    example: 'teste1@gmail.com',
+    example: 'teste@gmail.com',
   })
   @IsNotEmpty()
   @IsEmail()
