608fd368da79971ba8c37a29dc584ebb81316e8e
cezarfbc <cezarfbc@gmail.com>
2025-02-01 19:46:59 -0300

    refactor: simplify StripePaymentService and update BillingController methods

--- src/modules/billing/infrastructure/services/stripe_payment.service.ts
+++ src/modules/billing/infrastructure/services/stripe_payment.service.ts
@@ -1,39 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import Stripe from 'stripe';
 
 @Injectable()
-export class StripePaymentService {
-  private stripe: Stripe;
-
-  constructor() {
-    this.stripe = new Stripe('key', {
-      apiVersion: '2025-01-27.acacia',
-    });
-  }
-
-  async createCheckoutSession(
-    amount: number,
-    currency: string,
-  ): Promise<string> {
-    const session = await this.stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: [
-        {
-          price_data: {
-            currency: currency || 'brl',
-            product_data: {
-              name: 'Produto de Teste',
-            },
-            unit_amount: amount * 100,
-          },
-          quantity: 1,
-        },
-      ],
-      mode: 'payment',
-      success_url: 'http://localhost:8080/sucesso',
-      cancel_url: 'http://localhost:8080/cancelado',
-    });
-
-    return session.url;
-  }
-}
+export class StripePaymentService {}
--- src/modules/billing/interface/controllers/billing.controller.ts
+++ src/modules/billing/interface/controllers/billing.controller.ts
@@ -1,23 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { PaymentDto } from '../../application/dto/payment.dto';
-import { StripePaymentService } from '../../infrastructure/services/stripe_payment.service';
 import { IsPublicRoute } from 'src/common/decorators/is_public_route.decorator';
 
 @ApiTags('billing')
 @Controller('billing')
 export class BillingController {
-  constructor(private readonly stripePaymentService: StripePaymentService) {}
+  @IsPublicRoute()
+  @Post('payment/one-time')
+  public async oneTime() {}
 
   @IsPublicRoute()
-  @Post('payment/credit-card')
-  public async creditCardPayment(@Body() input: PaymentDto) {
-    const { amount, currency } = input;
-    const checkoutUrl = await this.stripePaymentService.createCheckoutSession(
-      amount,
-      currency,
-    );
+  @Post('payment/subscription')
+  public async subscription() {}
 
-    return { url: checkoutUrl };
-  }
+  @IsPublicRoute()
+  @Post('payment/webhook')
+  public async webhook() {}
 }
