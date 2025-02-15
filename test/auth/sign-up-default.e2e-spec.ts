5079d4a8770e5342aed95dc05b513ade0cb50ec5
cezarfbc <cezarfbc@gmail.com>
2025-02-15 20:10:51 -0300

    feat: refactor e2e tests for SignUpDefault to use AuthRepository and HashUtil mocks

--- test/auth/sign-up-default.e2e-spec.ts
+++ test/auth/sign-up-default.e2e-spec.ts
@@ -3,34 +3,63 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../../src/app.module';
 import { SignUpDefaultDto } from 'src/modules/auth/application/dto/sign_up_default.dto';
-import { SignUpDefaultUseCase } from 'src/modules/auth/application/use_cases/sign_up_default.use_case';
-import { ISignUpDefaultUseCase } from 'src/modules/auth/domain/interfaces/use_cases/sign_up.use_case.interface';
+import { IAuthRepository } from 'src/modules/auth/domain/interfaces/repositories/auth.repository.interface';
+import { Auth } from 'src/modules/auth/domain/entities/auth.entity';
+import { RolesEnum } from 'src/shared/enum/roles.enum';
+import { AuthRepository } from 'src/modules/auth/infrastructure/repositories/auth.repository';
+import { IHashUtil } from 'src/shared/utils/interfaces/hash.util.interface';
 
 describe('AuthController from AppModule (e2e)', () => {
   let app: INestApplication;
-  let signUpDefaultUseCaseMock: jest.Mocked<ISignUpDefaultUseCase>;
+  let authRepositoryMock: jest.Mocked<IAuthRepository>;
+  let hashUtilMock: jest.Mocked<IHashUtil>;
 
   beforeEach(async () => {
     if (app) {
       await app.close();
     }
 
-    signUpDefaultUseCaseMock = {
-      execute: jest.fn().mockImplementation((dto: SignUpDefaultDto) => {
-        return Promise.resolve({
-          id: '1',
-          email: dto.email,
-          newsletter_subscription: dto.newsletter_subscription,
-          terms_and_conditions_accepted: dto.terms_and_conditions_accepted,
-        });
-      }),
+    hashUtilMock = {
+      generateHash: jest.fn().mockResolvedValue('hashedText'),
+      compareHash: jest.fn().mockResolvedValue(true),
+    };
+
+    authRepositoryMock = {
+      create: jest
+        .fn()
+        .mockImplementation(
+          (
+            dto: SignUpDefaultDto,
+            code: string,
+            expires_at: Date,
+          ): Promise<Auth> => {
+            return Promise.resolve({
+              id: 1,
+              public_id: '1',
+              role: RolesEnum.USER,
+              email: dto.email,
+              password: 'hashedText',
+              is_verified_account: false,
+              newsletter_subscription: dto.newsletter_subscription,
+              terms_and_conditions_accepted: dto.terms_and_conditions_accepted,
+              created_at: new Date(),
+              updated_at: new Date(),
+            });
+          },
+        ),
+      findOneByEmail: jest.fn().mockResolvedValue(null),
+      updateInfoByIdAuth: jest.fn().mockResolvedValue(undefined),
+      updateInfoByPublicIdAuth: jest.fn().mockResolvedValue(undefined),
+      updateInfoByEmailAuth: jest.fn().mockResolvedValue(undefined),
     };
 
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     })
-      .overrideProvider(SignUpDefaultUseCase)
-      .useValue(signUpDefaultUseCaseMock)
+      .overrideProvider('IHashUtil')
+      .useValue(hashUtilMock)
+      .overrideProvider('IAuthRepository')
+      .useValue(authRepositoryMock)
       .compile();
 
     app = moduleFixture.createNestApplication();
@@ -45,26 +74,28 @@ describe('AuthController from AppModule (e2e)', () => {
       terms_and_conditions_accepted: true,
     };
 
+    const hashedText = 'hashedText';
+    const code = hashedText;
+
     const response = await request(app.getHttpServer())
       .post('/auth/sign-up-default/')
       .send(signUpData)
       .expect(201);
 
-    expect(response.body).toHaveProperty('id');
+    expect(response.body).not.toHaveProperty('id');
     expect(response.body).toHaveProperty('email', signUpData.email);
-    expect(response.body).toHaveProperty(
-      'password',
-      signUpData.email + signUpData.password,
+    expect(response.body).not.toHaveProperty('password');
+    expect(authRepositoryMock.create).toHaveBeenCalledWith(
+      {
+        ...signUpData,
+        password: hashedText,
+      },
+      code,
+      expect.objectContaining({
+        getTime: expect.any(Function),
+        toISOString: expect.any(Function),
+      }),
     );
-    expect(response.body).toHaveProperty(
-      'newsletter_subscription',
-      signUpData.newsletter_subscription,
-    );
-    expect(response.body).toHaveProperty(
-      'terms_and_conditions_accepted',
-      signUpData.terms_and_conditions_accepted,
-    );
-    expect(signUpDefaultUseCaseMock.execute).toHaveBeenCalledWith(signUpData);
   });
 
   it('Should return 409 when email already exists', async () => {
@@ -75,9 +106,17 @@ describe('AuthController from AppModule (e2e)', () => {
       terms_and_conditions_accepted: true,
     };
 
-    signUpDefaultUseCaseMock.execute.mockRejectedValueOnce({
-      statusCode: 409,
-      message: 'Unable to process request',
+    authRepositoryMock.findOneByEmail.mockResolvedValueOnce({
+      id: 1,
+      public_id: '1',
+      role: RolesEnum.USER,
+      email: signUpData.email,
+      password: 'hashedText',
+      is_verified_account: false,
+      newsletter_subscription: true,
+      terms_and_conditions_accepted: true,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     const response = await request(app.getHttpServer())
@@ -87,7 +126,7 @@ describe('AuthController from AppModule (e2e)', () => {
 
     expect(response.body).toHaveProperty('statusCode', 409);
     expect(response.body).toHaveProperty('message');
-    expect(signUpDefaultUseCaseMock.execute).toHaveBeenCalledWith(signUpData);
+    expect(authRepositoryMock.create).not.toHaveBeenCalled();
   });
 
   afterAll(async () => {
