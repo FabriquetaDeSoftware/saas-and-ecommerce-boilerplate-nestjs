920cd982bdd25c9f171398bd945a0b6827e3a483
cezarfbc <cezarfbc@gmail.com>
2025-02-15 20:26:58 -0300

    feat: enhance e2e tests for SignUpMagicLink by introducing AuthRepository and HashUtil mocks

--- test/auth/sign-up-magic-link.e2e-spec.ts
+++ test/auth/sign-up-magic-link.e2e-spec.ts
@@ -2,46 +2,124 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../../src/app.module';
-import { ISignUpMagicLinkUseCase } from 'src/modules/auth/domain/interfaces/use_cases/sign_up_magic_link.use_case.interface';
-import { SignUpMagicLinkUseCase } from 'src/modules/auth/application/use_cases/sign_up_magic_link.use_case';
 import { SignUpMagicLinkDto } from 'src/modules/auth/application/dto/sign_up_magic_link.dto';
+import { IAuthRepository } from 'src/modules/auth/domain/interfaces/repositories/auth.repository.interface';
+import { IHashUtil } from 'src/shared/utils/interfaces/hash.util.interface';
+import { Auth } from 'src/modules/auth/domain/entities/auth.entity';
+import { RolesEnum } from 'src/shared/enum/roles.enum';
 
 describe('AuthController from AppModule (e2e)', () => {
   let app: INestApplication;
-  let signUpMagicLinkUseCaseMock: jest.Mocked<ISignUpMagicLinkUseCase>;
+  let authRepositoryMock: jest.Mocked<IAuthRepository>;
+  let hashUtilMock: jest.Mocked<IHashUtil>;
 
   beforeEach(async () => {
     if (app) {
       await app.close();
     }
 
-    signUpMagicLinkUseCaseMock = {
-      execute: jest.fn().mockImplementation((dto: SignUpMagicLinkDto) => {
-        return Promise.resolve({
-          id: '1',
-          email: dto.email,
-          newsletter_subscription: dto.newsletter_subscription,
-          terms_and_conditions_accepted: dto.terms_and_conditions_accepted,
-        });
-      }),
+    hashUtilMock = {
+      generateHash: jest.fn().mockResolvedValue('hashedText'),
+      compareHash: jest.fn().mockResolvedValue(true),
+    };
+
+    authRepositoryMock = {
+      create: jest
+        .fn()
+        .mockImplementation(
+          (
+            dto: SignUpMagicLinkDto,
+            code: string,
+            expires_at: Date,
+          ): Promise<Auth> => {
+            return Promise.resolve({
+              id: 1,
+              public_id: '1',
+              role: RolesEnum.USER,
+              email: dto.email,
+              password: null,
+              is_verified_account: false,
+              newsletter_subscription: dto.newsletter_subscription,
+              terms_and_conditions_accepted: dto.terms_and_conditions_accepted,
+              created_at: new Date(),
+              updated_at: new Date(),
+            });
+          },
+        ),
+      findOneByEmail: jest.fn().mockResolvedValue(null),
+      updateInfoByIdAuth: jest.fn().mockResolvedValue(undefined),
+      updateInfoByPublicIdAuth: jest.fn().mockResolvedValue(undefined),
+      updateInfoByEmailAuth: jest.fn().mockResolvedValue(undefined),
     };
 
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     })
-      .overrideProvider(SignUpMagicLinkUseCase)
-      .useValue(signUpMagicLinkUseCaseMock)
+      .overrideProvider('IHashUtil')
+      .useValue(hashUtilMock)
+      .overrideProvider('IAuthRepository')
+      .useValue(authRepositoryMock)
       .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/auth/sign-up-magic-link/ (Post)', () => {
-    return request(app.getHttpServer())
+  it('Should return created user', async () => {
+    const signUpData: SignUpMagicLinkDto = {
+      email: 'test@example.com',
+      newsletter_subscription: true,
+      terms_and_conditions_accepted: true,
+    };
+
+    const code = 'hashedText';
+
+    const response = await request(app.getHttpServer())
       .post('/auth/sign-up-magic-link/')
-      .expect(200)
-      .expect('Hello World!');
+      .send(signUpData)
+      .expect(201);
+
+    expect(response.body).not.toHaveProperty('id');
+    expect(response.body).toHaveProperty('email', signUpData.email);
+    expect(response.body).not.toHaveProperty('password');
+    expect(authRepositoryMock.create).toHaveBeenCalledWith(
+      signUpData,
+      code,
+      expect.objectContaining({
+        getTime: expect.any(Function),
+        toISOString: expect.any(Function),
+      }),
+    );
+  });
+
+  it('Should return 409 when email already exists', async () => {
+    const signUpData: SignUpMagicLinkDto = {
+      email: 'existing@example.com',
+      newsletter_subscription: true,
+      terms_and_conditions_accepted: true,
+    };
+
+    authRepositoryMock.findOneByEmail.mockResolvedValueOnce({
+      id: 1,
+      public_id: '1',
+      role: RolesEnum.USER,
+      email: signUpData.email,
+      password: null,
+      is_verified_account: false,
+      newsletter_subscription: true,
+      terms_and_conditions_accepted: true,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    const response = await request(app.getHttpServer())
+      .post('/auth/sign-up-magic-link/')
+      .send(signUpData)
+      .expect(409);
+
+    expect(response.body).toHaveProperty('statusCode', 409);
+    expect(response.body).toHaveProperty('message');
+    expect(authRepositoryMock.create).not.toHaveBeenCalled();
   });
 
   afterAll(async () => {
