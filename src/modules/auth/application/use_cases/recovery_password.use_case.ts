4763ba8d3dc211692acec0d2782576db526bb5de
cezarfbc <cezarfbc@gmail.com>
2025-02-17 14:26:06 -0300

    feat: remove UnauthorizedException import from recovery password use case

--- src/modules/auth/application/use_cases/recovery_password.use_case.ts
+++ src/modules/auth/application/use_cases/recovery_password.use_case.ts
@@ -3,7 +3,6 @@ import {
   Inject,
   Injectable,
   NotFoundException,
-  UnauthorizedException,
 } from '@nestjs/common';
 import { RecoveryPasswordDto } from '../dto/recovery_password.dto';
 import { JwtService } from '@nestjs/jwt';
