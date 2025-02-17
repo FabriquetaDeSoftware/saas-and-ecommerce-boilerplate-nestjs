c413dcb78f0d8bcac6493d2fd570e4b02a04021c
cezarfbc <cezarfbc@gmail.com>
2025-02-17 14:25:16 -0300

    invalid funciona

--- test/auth/recovery-password.e2e-spec.ts
+++ test/auth/recovery-password.e2e-spec.ts
@@ -1,26 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, BadRequestException } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../../src/app.module';
 import { RecoveryPasswordDto } from 'src/modules/auth/application/dto/recovery_password.dto';
+import { JwtService } from '@nestjs/jwt';
+import { IJwtUserPayload } from 'src/shared/interfaces/jwt_user_payload.interface';
+import { TokenEnum } from 'src/shared/enum/token.enum';
 
 describe('AuthController from AppModule (e2e)', () => {
   let app: INestApplication;
+  let jwtServiceMock: jest.Mocked<JwtService>;
 
   beforeEach(async () => {
     if (app) {
       await app.close();
     }
 
+    jwtServiceMock = {
+      verify: jest
+        .fn()
+        .mockImplementation((token: string): Promise<IJwtUserPayload> => {
+          if (token === 'valid_token') {
+            return Promise.resolve({
+              email: Buffer.from('test@example.com').toString('base64'),
+              role: 'user',
+              sub: '123',
+              type: Buffer.from(TokenEnum.RECOVERY_PASSWORD_TOKEN).toString(
+                'base64',
+              ),
+            });
+          } else {
+            throw new BadRequestException('Invalid or expired token');
+          }
+        }),
+    } as any;
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(JwtService)
+      .useValue(jwtServiceMock)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('Should return password revovery', async () => {
+  it('Should return password recovery', async () => {
     const recoveryPasswordData: RecoveryPasswordDto = {
       token: 'valid_token',
       password: '123456',
@@ -31,7 +57,10 @@ describe('AuthController from AppModule (e2e)', () => {
       .send(recoveryPasswordData)
       .expect(200);
 
-    expect(response.body).toEqual({ message: 'Password recovered' });
+    expect(response.body).toHaveProperty(
+      'message',
+      'Password recovered successfully',
+    );
   });
 
   it('Should return 400 when token is invalid', async () => {
