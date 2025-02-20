cc78777ef3a0660fbc0f80e1a62f53147f7ae391
cezarfbc <cezarfbc@gmail.com>
2025-02-20 14:10:26 -0300

    feat: remove stock column and add slug to Products table; create Auth and VerificationCodes tables

--- prisma/migrations/20250220170957_init/migration.sql
+++ prisma/migrations/20250220170957_init/migration.sql
@@ -18,6 +18,21 @@ CREATE TABLE "Auth" (
 );
 
 -- CreateTable
+CREATE TABLE "Products" (
+    "id" SERIAL NOT NULL,
+    "public_id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" MONEY NOT NULL,
+    "slug" TEXT NOT NULL,
+    "image" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "VerificationCodes" (
     "id" SERIAL NOT NULL,
     "public_id" TEXT NOT NULL,
@@ -34,6 +49,12 @@ CREATE UNIQUE INDEX "Auth_public_id_key" ON "Auth"("public_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Auth_email_key" ON "Auth"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Products_public_id_key" ON "Products"("public_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Products_slug_key" ON "Products"("slug");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "VerificationCodes_public_id_key" ON "VerificationCodes"("public_id");
